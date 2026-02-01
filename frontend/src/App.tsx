d3e644351123391ba035421dc31ad7bbe0e84cd6
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-02-01 14:20:07 +0200

    init frontend react app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center gap-4 bg-sky-700 text-white">
+      <h1 className="text-4xl font-bold">Hello, World!</h1>
+      <p className="text-lg">
+        This is a React + TypeScript + Tailwind CSS v4 project.
+      </p>
+    </div>
+  );
+}
+
+export default App;
