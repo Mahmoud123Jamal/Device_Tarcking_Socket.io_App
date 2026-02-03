08074e21442354d2f7ac68df06fce608de7e8010
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-02-03 20:00:07 +0200

    feat(app): restructure App component to integrate RoomProvider and Home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
+import { RoomProvider } from "./context/RoomContext";
+import RootLayout from "./layout/RootLayout";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center gap-4 bg-sky-700 text-white">
-      <h1 className="text-4xl font-bold">Hello, World!</h1>
-      <p className="text-lg">
-        This is a React + TypeScript + Tailwind CSS v4 project.
-      </p>
-    </div>
+    <>
+      <RoomProvider>
+        <RootLayout>
+          <Home />
+        </RootLayout>
+      </RoomProvider>
+    </>
   );
 }
 
