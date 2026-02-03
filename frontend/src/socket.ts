fc170bf04f5827cf4c2e034a14aee939769fdd0d
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-02-03 11:29:00 +0200

    feat(socket): add functions for sending location updates and handling socket events

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -7,3 +7,20 @@ const socket = io(URL);
 export const joinRoom = (roomId: string) => {
   socket.emit("joinRoom", roomId);
 };
+
+export const sendLocationUpdate = (locationData: {
+  latitude: number;
+  longitude: number;
+}) => {
+  socket.emit("locationUpdate", locationData);
+};
+export const listenforLocationUpdate = (
+  callback: (locationData: { latitude: number; longitude: number }) => void,
+) => {
+  socket.on("userLeft", callback);
+};
+export const disconnectSocket = () => {
+  socket.disconnect();
+};
+
+export default socket;
