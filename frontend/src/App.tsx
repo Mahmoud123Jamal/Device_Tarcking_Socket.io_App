a94f59434e014ea336b93761e74bd2a6c2a8e0ed
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-02-16 21:56:27 +0200

    feat(routing): implement routing with Routes and Route components in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import { RoomProvider } from "./context/RoomContext";
 import RootLayout from "./layout/RootLayout";
 import Home from "./pages/Home";
+import Map from "./components/map";
 
 function App() {
   return (
-    <>
-      <RoomProvider>
-        <RootLayout>
-          <Home />
-        </RootLayout>
-      </RoomProvider>
-    </>
+    <RoomProvider>
+      <RootLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/room/:id" element={<Map />} />
+        </Routes>
+      </RootLayout>
+    </RoomProvider>
   );
 }
 
