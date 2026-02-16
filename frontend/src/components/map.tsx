805f796e402046f7066212746118d9052d77f268
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-02-16 22:56:53 +0200

    feat(map): add Map component with basic layout structure

---
+++ frontend/src/components/map.tsx
@@ -0,0 +1,10 @@
+function Map() {
+  return (
+    <div className="relative h-screen flex-col overflow-hidden ">
+      {/* top */}
+      <div className="sticky top-0 z-30 bg-linear-to-r from-sky-600 to-indigo-600 shadow-md p-4"></div>
+    </div>
+  );
+}
+
+export default Map;
